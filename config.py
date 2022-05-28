c4d43f05923eaa1fb4651527e6c19302c399725b
Philipp Bekher <blackrose00961@gmail.com>
2022-05-29 02:02:52 +0700

    add database

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = "5594270299:AAFidujPwvekgRaWlv2ahyoaKGlUOUDtDMA"
 APP_URL = "https://tg-bot13221.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://egximvumnvzgxs:e909a20e6b782bd9848fbda1e119a52daee4c35ee099f431c805ad7aec529f28@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/de4ro2sjke2qm5"
