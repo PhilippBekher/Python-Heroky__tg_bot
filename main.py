10e647466a3e7634d484ba0985c70a89022a2825
Philipp Bekher <blackrose00961@gmail.com>
2022-05-29 05:39:32 +0700

    sasa

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ db_object = db_connection.cursor()
 def start(message):
     id = message.from_user.id
     username = message.from_user.username
-    fullname = message.from_user.first_name + message.from_user.last_name
+    fullname = message.from_user.first_name + ' ' + message.from_user.last_name
 
     db_object.execute(f"SELECT id FROM users WHERE id = {id}")
     result = db_object.fetchone()
@@ -34,7 +34,7 @@ The test will take no more than 20 minutes😊
 Good luck🤞🏼""")
 
 
-        db_object.execute("INSERT INTO users(id, username, current_exercise) VALUES(%s,%s,%s)",(id, username, 1))
+        db_object.execute("INSERT INTO users(id, username, current_exercise) VALUES(%s,%s,%s,%s)",(id, username, 1, fullnam))
         db_connection.commit();
 
 
