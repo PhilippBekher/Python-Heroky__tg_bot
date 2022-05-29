ec53ab3636e67d902e089061626198751ac708a1
Philipp Bekher <blackrose00961@gmail.com>
2022-05-30 03:03:02 +0700

    assalsl

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "5594270299:AAFidujPwvekgRaWlv2ahyoaKGlUOUDtDMA"
 APP_URL = "https://tg-bot13221.herokuapp.com/" + BOT_TOKEN
 
-DB_URI = "postgres://egximvumnvzgxs:e909a20e6b782bd9848fbda1e119a52daee4c35ee099f431c805ad7aec529f28@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/de4ro2sjke2qm5"
+DB_URI = "postgres://cqqqjbdmyqojxe:1e7e9e0f765f1b06d918f78b0f5f0b95cc8008ce197aeb8fb23f2f1f377a0dfc@ec2-34-247-72-29.eu-west-1.compute.amazonaws.com:5432/d6fo20a8dj9hg7"
